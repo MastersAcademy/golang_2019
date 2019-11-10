e011c6308847c33a04d8703ffb451bd17b7a87bf
psixona <gorbatco.ivan@gmail.com>
2019-11-10 16:56:51 +0200

    1st commit

--- homeworks/ivan.horbatko-psixona/homework1/homework1.go
+++ homeworks/ivan.horbatko-psixona/homework1/homework1.go
@@ -12,23 +12,24 @@ import (
 
 // Ball declaration (4 / 3 * math.Pi * r * r *r
 type Ball struct {
+	//ballName string
 	r float64
 }
 
-//  Cone declaration (1/3*math.Pi*r*r*h)
+// Cone declaration (1/3*math.Pi*r*r*h)
 type Cone struct {
+	//coneName string
 	r, h float64
 }
 
 // Parallelepiped declaration (h * l * w)
 type Parallelepiped struct {
+	//parallelepipedName string
 	l, w, h float64
 }
 
 var (
-	b Ball
-	c Cone
-	p Parallelepiped
+	m = make(map[string]float64)
 )
 
 func (b *Ball) v() float64 {
@@ -41,4 +42,16 @@ func (c *Cone) v() float64 {
 
 func (p *Parallelepiped) v() float64 {
 	return p.h * p.l * p.w
+}
+
+func main() {
+	ball1 := Ball{1}
+	ball2 := Ball{1.5}
+	cone1 := Cone{5.0, 6.2}
+	parallelepiped1 := Parallelepiped{6, 9, 15}
+	m["ball1"] = ball1.v()
+	m["ball2"] = ball2.v()
+	m["cone1"] = cone1.v()
+	m["Parallelepiped1"] = parallelepiped1.v()
+	fmt.Println(m)
 }
