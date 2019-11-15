bb8cc13cd4ded0456feee718314a318d656089cf
psixona <gorbatco.ivan@gmail.com>
2019-11-15 20:17:18 +0200

    My hw1, I take idea with slice from Mezencev because my idea with map not working properly.

--- homeworks/ivan.horbatko-psixona/homework1/homework1.go
+++ homeworks/ivan.horbatko-psixona/homework1/homework1.go
@@ -3,6 +3,78 @@
 2. реалізувать список/контейнер для фігур (додавать можна різні фігури). він повинен сортувати свій вміст
 за обємом (для реалізації використовать інтерфейси)
 */
+//package main
+//
+//import (
+//	"fmt"
+//	"math"
+//)
+//
+//// Ball declaration (4 / 3 * math.Pi * r * r *r
+//type Ball struct {
+//	r float64
+//}
+//
+//// Cone declaration (1/3*math.Pi*r*r*h)
+//type Cone struct {
+//	r, h float64
+//}
+//
+//// Parallelepiped declaration (h * l * w)
+//type Parallelepiped struct {
+//	l, w, h float64
+//}
+//
+//func (b *Ball) V() float64 {
+//	return 4.0 / 3.0 * math.Pi * b.r * b.r * b.r
+//}
+//
+//func (c *Cone) V() float64 {
+//	return 1.0 / 3.0 * math.Pi * c.r * c.r * c.h
+//}
+//
+//func (p *Parallelepiped) V() float64 {
+//	return p.h * p.l * p.w
+//}
+//
+//var (
+//	m = make(map[string]interface{})
+//	mList = make(map[string]float64)
+//)
+//
+//type Messier interface {
+//	V() float64
+//}
+//
+//type List []Messier
+//
+//
+//func (s List) Len() int {
+//	return len(s)
+//}
+//func (s List) Swap(i, j int) {
+//	s[i], s[j] = s[j], s[i]
+//}
+//func (s List) Less(i, j int) bool {
+//	return s[i].V() < s[j].V()
+//}
+//
+//func main() {
+//	m["Ball"] = &Ball{r: 4}
+//	m["Ball2"] = &Ball{r: 2}
+//	m["Cone"] = &Cone{r: 5, h: 6}
+//	m["Parallelepiped"] = &Parallelepiped{8, 3, 4.2}
+//	m["Parallelepiped2"] = &Parallelepiped{6, 4, 4.8}
+//	m["Cone2"] = &Cone{r: 6, h: 5}
+//	for key, value := range m {
+//		strKey := fmt.Sprintf("%v", key)
+//		strValue, _ := fmt.Printf("%v", value)
+//		mList[strKey] = float64(strValue)
+//	}
+//	fmt.Println(m)
+//	fmt.Printf("%#v", mList)
+//}
+
 package main
 
 import (
@@ -13,69 +85,62 @@ import (
 
 // Ball declaration (4 / 3 * math.Pi * r * r *r
 type Ball struct {
-	ballName string
-	r        float64
+	r float64
 }
 
 // Cone declaration (1/3*math.Pi*r*r*h)
 type Cone struct {
-	coneName string
-	r, h     float64
+	r, h float64
 }
 
 // Parallelepiped declaration (h * l * w)
 type Parallelepiped struct {
-	parallelepipedName string
-	l, w, h            float64
+	l, w, h float64
 }
 
-var (
-	m = make(map[string]float64)
-)
-
-func (b *Ball) v() float64 {
+func (b *Ball) V() float64 {
 	return 4.0 / 3.0 * math.Pi * b.r * b.r * b.r
 }
 
-func (c *Cone) v() float64 {
+func (c *Cone) V() float64 {
 	return 1.0 / 3.0 * math.Pi * c.r * c.r * c.h
 }
 
-func (p *Parallelepiped) v() float64 {
+func (p *Parallelepiped) V() float64 {
 	return p.h * p.l * p.w
 }
 
+var m = make(List, 0, 6)
+
+type Messier interface {
+	V() float64
+}
+
+type List []Messier
+
+func receiver(shapes List) {
+	for _, s := range shapes {
+		fmt.Printf("Shape: %#v = %v\n", s, s.V())
+	}
+}
+
+func (s List) Len() int {
+	return len(s)
+}
+func (s List) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s List) Less(i, j int) bool {
+	return s[i].V() < s[j].V()
+}
+
 func main() {
-	ball1 := Ball{"ball1", 4.6}
-	ball2 := Ball{"ball2", 5.7}
-	cone1 := Cone{"cone1", 5.0, 6.2}
-	cone2 := Cone{"cone2", 3.3, 8.8}
-	parallelepiped1 := Parallelepiped{"parallelepiped1", 6.3, 9.2, 15}
-	parallelepiped2 := Parallelepiped{"parallelepiped2", 8, 3, 4.4}
-	m[ball1.ballName] = ball1.v()
-	m[ball2.ballName] = ball2.v()
-	m[cone1.coneName] = cone1.v()
-	m[cone2.coneName] = cone2.v()
-	m[parallelepiped1.parallelepipedName] = parallelepiped1.v()
-	m[parallelepiped2.parallelepipedName] = parallelepiped2.v()
-	fmt.Println(m)
-	type sSort struct {
-		Key   string
-		Value float64
-	}
-
-	var ss []sSort
-	for k, v := range m {
-		ss = append(ss, sSort{k, v})
-		//fmt.Println(ss)
-	}
-
-	sort.Slice(ss, func(i, j int) bool {
-		//fmt.Println(ss[i].Value, ss[j].Value, i , j)
-		return ss[i].Value > ss[j].Value
-	})
-
-	for _, sSort := range ss {
-		fmt.Printf("%s, %v\n", sSort.Key, sSort.Value)
-	}
+	m = append(m, &Ball{10},
+		&Cone{5, 8},
+		&Parallelepiped{2, 3, 3},
+		&Ball{6},
+		&Cone{2.2, 8},
+		&Parallelepiped{3.5, 2.2, 4.4})
+	sort.Sort(m)
+	receiver(m)
 }
