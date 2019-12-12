d38c43b7bf2efd8c5df8dbdaf6267a621b62e356
nvm <Never M>
2019-12-12 11:17:19 +0200

    Implement order

--- homeworks/Frank.Teardrop-i-hate-nicknames/homework3/order.go
+++ homeworks/Frank.Teardrop-i-hate-nicknames/homework3/order.go
@@ -2,21 +2,56 @@ package main
 
 import(
     "strings"
+    "strconv"
 )
 
 type Order []int
 
-// create an order except one instance of given item
+// create an order except one instance of the given item
+// if the order has multiple instances, only one will be removed
 func (o Order) except(item int) Order {
-    return o
+    found := false
+    result := make([]int, 0)
+    for _, orderItem := range o {
+        if !found && item == orderItem {
+            found = true
+            continue
+        }
+        result = append(result, orderItem)
+    }
+    return result
 }
 
 func MakeOrder(s string) Order {
-    xs := make([]int, 0)
-    
-    return xs
+    items := make([]int, 0)
+    numbers := strings.TrimSpace(s[1:len(s)-1])
+    itemsStr := strings.Split(numbers, " ")
+    if len(numbers) == 0 {
+        return items
+    }
+    for _, itemStr := range itemsStr {
+        item, err := strconv.Atoi(itemStr)
+        if err != nil {
+            panic("Malformed order specification: " + s)
+        }
+        items = append(items, item)
+    }
+    return items
 }
 
 func (o Order) String() string {
-    return ""
+    var sb strings.Builder
+    sb.WriteString("{")
+    for idx, item := range o {
+        sb.WriteString(strconv.Itoa(item))
+        if (idx < len(o)-1) {
+            sb.WriteString(" ")
+        }
+    }
+    sb.WriteString("}")
+    return sb.String()
+}
+
+func (o Order) Equals(other Order) bool {
+    return strings.Compare(o.String(), other.String()) == 0
 }
