81d6526b5b2fa373be9760c096926073d839d77a
nvm <Never M>
2019-12-12 10:38:26 +0200

    Add order and vending function stubs

---
+++ homeworks/Frank.Teardrop-i-hate-nicknames/homework3/order.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+    "strings"
+)
+
+type Order []int
+
+// create an order except one instance of given item
+func (o Order) except(item int) Order {
+    return o
+}
+
+func MakeOrder(s string) Order {
+    xs := make([]int, 0)
+    
+    return xs
+}
+
+func (o Order) String() string {
+    return ""
+}
---
+++ homeworks/Frank.Teardrop-i-hate-nicknames/homework3/vending.go
@@ -0,0 +1,11 @@
+package main
+
+type State []Machine
+
+func ProcessOrder(order []int, state State) (State, bool) {
+    return nil, false
+}
+
+func processItem(item int, state State) []State {
+    return nil
+}
